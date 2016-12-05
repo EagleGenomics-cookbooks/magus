5bbf18a881b90d4dea0cf0174ad460e2c027d47f
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-12-05 12:35:22 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# magus
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'magus'
+maintainer 'Eagle Genomics Ltd'
+maintainer_email 'chef@eaglegenomics.com'
+license 'apachev2'
+description 'Installs/Configures magus'
+long_description 'Installs/Configures magus'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/magus/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/magus' if respond_to?(:source_url)
